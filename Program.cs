a6a76f3f478535ea59a435f852abb1143dfb32ec
RafalPietrzak55 <115399790+RafalPietrzak55@users.noreply.github.com>
2025-05-28 18:44:45 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+class IrisSample
+{
+    public double[] Features;
+    public int Label;
+}
+
+class Program
+{
+    static void Main()
+    { 
+    
+    
+    }
+}
