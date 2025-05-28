a1e02f29081273206a6a412946af3e20b696dc9c
RafalPietrzak55 <115399790+RafalPietrzak55@users.noreply.github.com>
2025-05-28 19:01:36 +0200

    Samples

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
-
 
 class IrisSample
 {
@@ -13,8 +13,18 @@ class IrisSample
 class Program
 {
     static void Main()
-    { 
-    
-    
+    {
+        var samples = File.ReadAllLines("iris.txt")
+            .Where(line => !string.IsNullOrWhiteSpace(line))
+            .Select(line => line.Split('\t'))
+            .Select(parts => new IrisSample
+            {
+                Features = parts.Take(4)
+                                .Select(s => double.Parse(s, CultureInfo.InvariantCulture))
+                                .ToArray(),
+                Label = int.Parse(parts[4])
+            }).ToList();
+
+        Console.WriteLine($"Wczytano {samples.Count} próbek.");
     }
 }
